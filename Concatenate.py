2453a3d9eeee903e42a8bd41d3cfad1c8157b95e
J.C. Martel <jmartel701@hotmail.com>
2016-02-25 14:02:13 -0600

    Added files via upload

---
+++ Concatenate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 14:42:46 2016
+
+@author: jmartel71
+"""
+
---
+++ Set.Working.Directory.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 13 08:54:40 2016
+
+@author: jmartel71
+"""
+
+import os
+os.chdir('C:/Users/jmartel71/Desktop/Python Scripts/')
+os.getcwd()
